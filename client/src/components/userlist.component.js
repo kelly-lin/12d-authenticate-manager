70089acc56ae13e095f9255b3a4d23a10681f114
Kelly Lin <findkellylin@gmail.com>
2021-01-05 22:40:23 +1100

    Refactor userlist to userList

--- client/src/components/userlist.component.js
+++ client/src/components/userlist.component.js
@@ -1,7 +1,58 @@
 import React, { Component } from 'react';
 import Table from 'react-bootstrap/Table';
+import axios from 'axios';
 
-export default class Userlist extends Component {
+const getUserStatus = user => {
+  const accessLevels = {
+    admin: { code: 0, text: 'Admin'},
+    restricted: { code: 1, text: 'Restricted' },
+    revoked: { code: 2, text: 'Revoked' }
+  }
+
+  if(user.accessLevel === accessLevels.admin.code) {
+    return accessLevels.admin.text;
+  } else if(user.accessLevel === accessLevels.restricted.code) {
+    return accessLevels.restricted.text;
+  } else if(user.accessLevel === accessLevels.revoked.code) {
+    return accessLevels.revoked.text;
+  }
+  
+  return "Error! Code undefined";
+}
+
+const User = props => {
+ return (
+  <tr>
+    <td>{ props.user.username }</td>
+    <td>{ props.user.name }</td>
+    <td>{ getUserStatus(props.user) }</td>
+    <td>31/12/2020</td>
+    <td>Approved</td>
+  </tr>
+ );
+}
+
+export default class UserList extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.userList = this.userList.bind(this);
+    this.state = { users: [] };
+  }
+
+  userList() {
+    return this.state.users.map(user => {
+      return <User user={user} />;
+    });
+  }
+
+  componentDidMount() {
+    axios.get('/users')
+      .then(res => {
+        this.setState({ users: res.data });
+      });
+  }
+  
   render() {
     return (
       <Table striped bordered hover variant="dark">
@@ -15,13 +66,7 @@ export default class Userlist extends Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>AUKL500446</td>
-            <td>Lin, Kelly</td>
-            <td>Admin</td>
-            <td>31/12/2020</td>
-            <td>Approved</td>
-          </tr>
+          { this.userList() }
         </tbody>
       </Table>
     );
