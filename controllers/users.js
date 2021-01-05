312d9fceb4aa715394276c3d2488a3d51e28e810
Kelly Lin <findkellylin@gmail.com>
2021-01-05 23:30:03 +1100

    refactor error function

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,10 @@
 let User = require('../models/user.model');
+const error = require('../services/error');
 
 function getUsers(req, res) {
   User.find()
     .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error! ' + err));
+    .catch(error.throw());
 }
 module.exports.getUsers = getUsers;
 
@@ -25,6 +26,6 @@ function addUser(req, res) {
   
   newUser.save()
   .then(() => res.json('User added!'))
-  .catch(err => res.status(400).json('Error! ' + err));
+  .catch(err => res.status(400).json(error.throw()));
 }
 module.exports.addUser = addUser;
