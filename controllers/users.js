fb89a07543e51137dc6ee9806a57759ef1da4d22
Kelly Lin <findkellylin@gmail.com>
2021-01-05 23:11:19 +1100

    add addUser controller

---
+++ controllers/users.js
@@ -0,0 +1,30 @@
+let User = require('../models/user.model');
+
+function getUsers(req, res) {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error! ' + err));
+}
+module.exports.getUsers = getUsers;
+
+function addUser(req, res) {
+  const username = req.body.username;
+  const name = req.body.name;
+  const email = req.body.email;
+  const projectApproved = req.body.pName;
+  const projectCode = req.body.pCode;
+  
+  const newUser = new User({
+    username: username,
+    name: name,
+    email: email,
+    projectApproved: projectApproved,
+    projectCode: projectCode,
+    isPending: true
+  });
+  
+  newUser.save()
+  .then(() => res.json('User added!'))
+  .catch(err => res.status(400).json('Error! ' + err));
+}
+module.exports.addUser = addUser;
