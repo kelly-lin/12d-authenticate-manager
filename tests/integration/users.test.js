97b22fa985b1482a25b6887af5cd2fb7de293f07
Kelly Lin <findkellylin@gmail.com>
2021-01-07 00:39:25 +1100

    refactor for database fixture

--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -3,64 +3,41 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const User = require('../../models/user.model');
 const request = require('supertest');
 const app = require('../../app');
+const db = require('../fixtures/database');
 
 // May require additional time for downloading MongoDB binaries
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
 
-let mongoServer;
-const opts = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-};
-
 beforeAll(async () => {
-  mongoServer = new MongoMemoryServer();
-  const mongoUri = await mongoServer.getUri();
-  await mongoose.connect(mongoUri, opts, (err) => {
-    if (err) console.error(err);
-  });
-
-  const newUser1 = new User({
-    username: 'AUKL500446',
-    name: 'Lin,Kelly',
-    email: 'Kelly.Lin@wsp.com',
-    projectApproved: 'Appin Road',
-    projectCode: 'PS158157319'
-  });
-  await newUser1.save();
-
-  const newUser2 = new User({
-    username: 'AURM500508',
-    name: 'McNeil,Roisin',
-    email: 'Roisin.McNeill@wsp.com',
-    projectApproved: 'PLR',
-    projectCode: 'PS158157315'
-  });
-  await newUser2.save();
+  await db.connect();
 });
 
 afterAll(async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
+  await db.disconnect();
 });
 
+afterEach(async () => {
+  await db.clear();
+})
+
 test('Get all users returns 2', async () => {
+  await new User(db.users.user1).save();
+  await new User(db.users.user2).save();
   const data = await request(app).get('/users');
+
   expect(data.body.length).toBe(2);
 });
 
 test('Add user', async () => {
-  const newUser = {
-    username: 'John.Appleseed',
-    name: 'Appleseed,John',
-    email: 'john.appleseed@john.com',
-    pName: 'dt',
-    pCode: 'dt'
-  }
+  const numOfUsersStart = await User.find().countDocuments((err, count) => {
+    if(err) console.log(err);
+    return count;
+  });
+
+  expect(numOfUsersStart).toBe(0);
 
   await request(app).post('/users/add')
-    .send(newUser)
+    .send(db.users.user1)
     .catch(err => console.log('Error!: ' + err));
   
   const numOfUsers = await User.find().countDocuments((err, count) => {
@@ -68,5 +45,13 @@ test('Add user', async () => {
     return count;
   });
 
-  expect(numOfUsers).toBe(3);
+  expect(numOfUsers).toBe(1);
+});
+
+test('add pending', async () => {
+  let users = await request(app).get('/pending').then(result => result.body);
+  expect(users.length).toBe(0);
+  await new User(db.users.user1).save();
+  users = await request(app).get('/pending').then(result => result.body);
+  expect(users.length).toBe(1);
 });
