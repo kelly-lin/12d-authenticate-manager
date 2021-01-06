b7f9db10c609e7f68a06e5cf0a3c102983fe9f0a
Kelly Lin <findkellylin@gmail.com>
2021-01-07 00:48:32 +1100

    refactor controller functions

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,8 @@
 const router = require('express').Router();
-let User = require('../models/user.model');
-
-const getUsers = require('../controllers/getUsers');
+const userContollers = require('../controllers/users');
 
 // Get all the users from the database
-router.route('/').get(getUsers);
-
-router.route('/add').post((req, res) => {
-  const username = req.body.username;
-  const name = req.body.name;
-  const email = req.body.email;
-  const projectApproved = req.body.pName;
-  const projectCode = req.body.pCode;
-
-  const newUser = new User({
-    username: username,
-    name: name,
-    email: email,
-    projectApproved: projectApproved,
-    projectCode: projectCode
-  });
-  
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error! ' + err));
-});
+router.route('/').get(userContollers.getUsers);
+router.route('/add').post(userContollers.addUser);
 
 module.exports = router;
