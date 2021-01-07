fae62b5865df8f0fa234e884b3b97d5666cf193c
Kelly Lin <findkellylin@gmail.com>
2021-01-07 13:40:06 +1100

    removed redundant comment

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const userContollers = require('../controllers/users');
 
-// Get all the users from the database
 router.route('/').get(userContollers.getUsers);
 router.route('/add').post(userContollers.addUser);
 router.route('/pending').get(userContollers.getPendingUsers);
