a4849d9d1d40eabf142b0c464402b5342919af2c
Kelly Lin <findkellylin@gmail.com>
2021-01-08 20:59:48 +1100

    add form and submission function

--- client/src/components/approve-user-form.component.js
+++ client/src/components/approve-user-form.component.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import { accessLevels } from '../services/users';
+import axios from 'axios';
+
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import Form from 'react-bootstrap/Form';
 
-const PermissionLevelButtons = props => {  
-  const radios = [
-    { name: 'Admin', value: '1' },
-    { name: 'Restricted - 1 month', value: '2' },
-    { name: 'Restricted - 6 months', value: '3' },
-    { name: 'Restricted - 12 months', value: '4' },
-  ];
+const permissionRadioValues = {
+  admin: '1',
+  restricted: {
+    oneMonth: '2',
+    sixMonths: '3',
+    twelveMonths: '4'
+  }
+}
 
+const radios = [
+  { name: 'Admin', value: permissionRadioValues.admin },
+  { name: 'Restricted - 1 month', value: permissionRadioValues.restricted.oneMonth },
+  { name: 'Restricted - 6 months', value: permissionRadioValues.restricted.sixMonths },
+  { name: 'Restricted - 12 months', value: permissionRadioValues.restricted.twelveMonths },
+];
+
+const PermissionLevelButtons = props => {
   return radios.map((radio, idx) => {
     return <ToggleButton
               key={ idx }
@@ -30,28 +42,82 @@ export default class ApproveUserForm extends Component {
   constructor(props) {
     super(props);
     this.state = {}
+    this.onSubmit = this.onSubmit.bind(this);
     this.onChangeUserPermissions = this.onChangeUserPermissions.bind(this);
   }
-  
+
   onChangeUserPermissions(e) {
     this.setState({ radioValue: e.currentTarget.value });
   }
 
+  onSubmit(e) {
+    let accessLevel = null;
+    let numOfMonths = null;
+
+    switch(this.state.radioValue) {
+      case permissionRadioValues.admin:
+        accessLevel = accessLevels.admin.code;
+        break;
+
+      case permissionRadioValues.restricted.oneMonth:
+        accessLevel = accessLevels.restricted.code;
+        numOfMonths = 1;
+        break;
+
+      case permissionRadioValues.restricted.sixMonths:
+        accessLevel = accessLevels.restricted.code;
+        numOfMonths = 6;
+        break;
+
+      case permissionRadioValues.restricted.twelveMonths:
+        accessLevel = accessLevels.restricted.code;
+        numOfMonths = 12;
+        break;
+
+      default:
+        accessLevel = null;
+        numOfMonths = 0;
+    }
+
+    if(accessLevel != null) {
+      this.props.user.accessLevel = accessLevel;
+
+      if(accessLevel === accessLevels.restricted.code){
+        let todayDate = new Date();
+        let expiryDate = new Date(
+          todayDate.setMonth(
+            todayDate.getMonth() + numOfMonths
+          )
+        );
+        this.props.user.expiryDate = expiryDate;
+      }
+
+      this.props.user.isPending = false;
+      console.log(this.props.user);
+      const data = this.props.user;
+      axios.post('/users/update/' + this.props.user._id, data);
+    }
+  }
+
   render() {
     return (
-      <>
-        <form>
-          <Form.Group controlId="userPermissions">
-            <Form.Label>Please select user permissions</Form.Label>
-            <ButtonGroup toggle>
-            <PermissionLevelButtons 
-              onChangeUserPermissions={ this.onChangeUserPermissions }
-              radioValue={ this.state.radioValue }
-            />
-            </ButtonGroup>
-          </Form.Group>
-        </form>
-      </>
+      <form
+        id="user-permissions-form"
+        onSubmit={ () => {
+          this.onSubmit();
+          this.props.handleHideUser(this.props.user._id);
+        }}
+      >
+        <Form.Group controlId="userPermissions">
+          <Form.Label>Please select user permissions</Form.Label>
+          <ButtonGroup toggle>
+          <PermissionLevelButtons
+            onChangeUserPermissions={ this.onChangeUserPermissions }
+            radioValue={ this.state.radioValue }
+          />
+          </ButtonGroup>
+        </Form.Group>
+      </form>
     )
   }
 }
