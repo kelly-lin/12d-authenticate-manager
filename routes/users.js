58c47781d3ca0573b819d49227d7fff6a2648f4d
Kelly Lin <findkellylin@gmail.com>
2021-01-08 20:57:12 +1100

    add update user route

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const userContollers = require('../controllers/users');
 router.route('/').get(userContollers.getUsers);
 router.route('/add').post(userContollers.addUser);
 router.route('/pending').get(userContollers.getPendingUsers);
+router.route('/update/:id').post(userContollers.updateUser);
 
 module.exports = router;
